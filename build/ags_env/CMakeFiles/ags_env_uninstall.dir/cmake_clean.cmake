a28d57883883826a1a8507c9f11b214c74bc8a9d
Moksh Kandhari <169137952+moksh-kandhari@users.noreply.github.com>
2025-06-05 14:56:28 +0530

    Delete build directory

--- build/ags_env/CMakeFiles/ags_env_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ags_env_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ags_env_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
